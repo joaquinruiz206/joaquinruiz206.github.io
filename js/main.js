c3e948b80195cf2f1f58673c59020d508b44312a
Joaquín Francisco Ruiz Orellano <joaquinruizorellano206@gmail.com>
2022-10-18 17:55:01 -0300

    Datos

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openSubmenu(element){
+    console.log(element);
+    document.getElementsByClassName("submenu")[0].classList.toggle("submenuUnhide");
+}
+
+function openSubmenuPlanet(element){
+    console.log(element);
+    document.getElementsByClassName("submenu")[0].classList.toggle("submenuUnhide-Planets");
+}
